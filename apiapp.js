dc22eec62c5309ae0ebac2359e5fb4bb004c327e
udityaprakash <udityaprakash983@gmail.com>
2022-12-26 01:18:49 +0530

    msg changed

--- apiapp.js
+++ apiapp.js
@@ -3,20 +3,24 @@ const bodyparser = require("body-parser");
 const app = express();
 const mongoose = require("mongoose");
 const dburl = "mongodb+srv://udityaprakash01:sAMc1FmiB4wWnxAx@cluster0.za5wk8j.mongodb.net/?retryWrites=true&w=majority";
-mongoose.connect(dburl+"dbtest",(err)=>{
+mongoose.connect(dburl,(err)=>{
     if(err){
         console.log(err);
     }else{
         console.log("success");
     }
 });
-const se = {
-    status: "Success",
-    msg:"Yeh made my first API"
+
+
+const send = {
+    status: {code: 200,msg:"Ok Status"},
+    msg:"Api is ready to Serve"
 }
 app.get("/",(req,res)=>{
-    res.json(se);
+    res.json(send);
 });
+
+
 
 app.listen(3000 ,()=>{
     console.log("server started");
